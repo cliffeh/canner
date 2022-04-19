6d8b12a5322da8865caa7d5d33f86b979ec3197e
Cliff Snyder <cliff.snyder@gmail.com>
2022-04-18 22:04:25 -0400

    dice testing for dynamic examples

--- examples/test/test-dice.c
+++ examples/test/test-dice.c
@@ -1,5 +1,7 @@
 #include <assert.h>
-#define NROLLS 1024
+#include <stdio.h>
+
+#define NROLLS 4096
 #define NSIDES 6
 
 int roll_dice (int result[], int rolls, int sides);
@@ -7,13 +9,19 @@ int roll_dice (int result[], int rolls, int sides);
 int
 main ()
 {
-  int results[NROLLS], i;
+  int rolls[NROLLS] = { 0 }, i, results[NSIDES] = { 0 };
   // NROLLSd6
-  roll_dice (results, NROLLS, NSIDES);
+  roll_dice (rolls, NROLLS, NSIDES);
   for (i = 0; i < NROLLS; i++)
     {
-        assert(1 <= results[i]);
-        assert(results[i] <= NSIDES);
-        // TODO test for fair distribution
+      assert (1 <= rolls[i]);
+      assert (rolls[i] <= NSIDES);
+      results[rolls[i] - 1]++;
+    }
+
+  // TODO check for fairness
+  for (i = 0; i < NSIDES; i++)
+    {
+      printf ("%i: %i\n", i + 1, results[i]);
     }
 }
