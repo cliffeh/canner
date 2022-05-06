9d4f36067e2a22784a7668f48466c2bb6cdeb521
Cliff Snyder <cliff.snyder@gmail.com>
2022-05-06 14:20:38 -0400

    beginning option parsing

--- canner.c
+++ canner.c
@@ -1,4 +1,5 @@
 #include "config.h"
+#include <errno.h>
 #include <limits.h>
 #include <popt.h>
 #include <stdio.h>
@@ -7,8 +8,6 @@
 #include <sys/queue.h>
 #include <sys/types.h>
 #include <unistd.h>
-
-#define USAGE_ARGS "DIR [HEADERS] [-h|--help]"
 
 static const char *main_template[] = {
 #include "main.h"
@@ -30,53 +29,48 @@ main (int argc, const char *argv[])
 {
   struct callback_entry *cb;
   char path[PATH_MAX] = { 0 }, *prefix = "";
+  const char *dir;
   int i, rc;
   FILE *c_out = stdout;
 
   char *c_out_name = "-";
 
-  // poptContext optCon;
-  // struct poptOption options[]
-  //     = { /* longName, shortName, argInfo, arg, val, descrip, argDescript */
-  //         { "outfile", 'o', POPT_ARG_STRING | POPT_ARGFLAG_SHOW_DEFAULT,
-  //           &c_out_name, 'o', "specify output file name", "FILE" },
-  //         POPT_AUTOHELP POPT_TABLEEND
-  //       };
+  poptContext optCon;
+  struct poptOption options[]
+      = { /* longName, shortName, argInfo, arg, val, descrip, argDescript */
+          { "outfile", 'o', POPT_ARG_STRING | POPT_ARGFLAG_SHOW_DEFAULT,
+            &c_out_name, 'o', "specify output file name", "FILE" },
+          POPT_AUTOHELP POPT_TABLEEND
+        };
 
-  // optCon = poptGetContext (0, argc, argv, options, 0);
-  // while ((rc = poptGetNextOpt (optCon)) > 0)
-  //   {
-  //     // printf ("option: %i\n", rc);
-  //   }
+  optCon = poptGetContext (0, argc, argv, options, 0);
+  poptSetOtherOptionHelp (optCon, "[OPTION...] DIR\nOptions:");
 
-  // if (rc != -1)
-  //   {
-  //     fprintf (stderr, "error: %s: %s\n",
-  //              poptBadOption (optCon, POPT_BADOPTION_NOALIAS),
-  //              poptStrerror (rc));
-  //     poptPrintHelp (optCon, stderr, 0);
-  //     poptFreeContext (optCon);
-  //     exit (1);
-  //   }
-
-  // printf ("outname: %s\n", c_out_name);
-
-  if (argc < 2)
+  while ((rc = poptGetNextOpt (optCon)) > 0)
     {
-      fprintf (stderr, "usage: %s " USAGE_ARGS "\n", argv[0]);
+    }
+
+  if (rc != -1)
+    {
+      fprintf (stderr, "error: %s: %s\n",
+               poptBadOption (optCon, POPT_BADOPTION_NOALIAS),
+               poptStrerror (rc));
+      poptPrintHelp (optCon, stderr, 0);
+      poptFreeContext (optCon);
       exit (1);
     }
 
-  for (i = 1; i < argc; i++)
+  dir = poptGetArg (optCon);
+  if (!dir || poptGetArg (optCon))
     {
-      if (strcmp ("--help", argv[i]) == 0 || strcmp ("-h", argv[i]) == 0)
-        {
-          fprintf (stdout, "usage: %s " USAGE_ARGS "\n", argv[0]);
-          exit (0);
-        }
+      // TODO allow multiple directories to be passed in?
+      fprintf (stderr, "error: exactly one directory name must be provided\n");
+      poptPrintHelp (optCon, stderr, 0);
+      poptFreeContext (optCon);
+      exit (1);
     }
 
-  sprintf (path, "%s", argv[1]);
+  sprintf (path, "%s", dir);
   // ensure a single trailing slash
   while (path[strlen (path) - 1] == '/')
     {
@@ -84,18 +78,12 @@ main (int argc, const char *argv[])
     }
   path[strlen (path)] = '/';
 
-  // include any extra headers
-  for (i = 2; i < argc; i++)
+  c_out = (strcmp (c_out_name, "-") == 0) ? stdout : fopen (c_out_name, "w");
+  if (!c_out)
     {
-      switch (argv[i][0])
-        {
-        case '<':
-        case '"':
-          fprintf (c_out, "#include %s\n", argv[i]);
-          break;
-        default:
-          fprintf (c_out, "#include \"%s\"\n", argv[i]);
-        }
+      fprintf (stderr, "error: unable to open output file: %s (%s)\n",
+               c_out_name, strerror (errno));
+      exit (1);
     }
 
   fprintf (c_out, "%s\n", main_template[0]);
